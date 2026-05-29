7b64c75426e7fa9ffd7b197c0d9d56336561bb03
Vidyashree L S <vidyashreels181202@gmail.com>
2026-05-29 16:46:54 +0000

    Sync LeetCode submission - Maximum Product of Three Numbers - Runtime - 21 ms (46.24%), Memory - 20.4 MB (18.95%)

---
+++ leetcode/0628-maximum-product-of-three-numbers/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+        m = max(nums[n-1] * nums[n-2] * nums[n-3], nums[0] * nums[1] * nums[n-1])
+        return m
